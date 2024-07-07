4fe83495de58e0c0114498e9a797d4eee201d7a1
Alien <lmllewellyn@gmail.com>
2024-07-07 09:58:03 -0400

    added prompts in the promise chain, installed inquirer package

--- index.js
+++ index.js
@@ -1,7 +1,63 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "Enter the title",
+            default: "My README",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Enter description",
+            default: "This is a README",
+        },
+        {
+            type: "input",
+            name: "contents",
+            message: "Enter table of contents",
+            default: "This is my table of contents",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Enter installation instructions",
+            default: "This is what you need to install to use",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Enter usage instructions",
+            default: "This is how you use this app",
+        },
+        {
+            type: "checkbox",
+            name: "license",
+            message: "Pick the necessary license",
+            choices: [],
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "How can users contribute to this current app",
+            default: "Here is how you can contribute to this app",
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "What tests need to be run for this app",
+            default: "These are the tests necessary to run this app",
+        }
+    ])
+    .then()
+    .then()
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
